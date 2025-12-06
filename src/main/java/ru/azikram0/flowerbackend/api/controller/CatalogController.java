fb6b144a9d28428de53ce5709d249e126d49414b
Elizaveta <elisavetayapryntseva@yandex.ru>
2025-12-06 14:25:11 +0400

    All ready

--- src/main/java/ru/azikram0/flowerbackend/api/controller/CatalogController.java
+++ src/main/java/ru/azikram0/flowerbackend/api/controller/CatalogController.java
@@ -1,46 +1,68 @@
 package ru.azikram0.flowerbackend.api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import ru.azikram0.flowerbackend.api.dto.FlowerCardDto;
 import ru.azikram0.flowerbackend.api.dto.FlowerDto;
 import ru.azikram0.flowerbackend.api.dto.PriceRangeDto;
+import ru.azikram0.flowerbackend.domain.service.Catalog;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
 @RequestMapping("/catalog")
 public class CatalogController {
+    private final Catalog catalog;
+
+    @Autowired
+    public CatalogController(Catalog catalog) {
+        this.catalog = catalog;
+    }
+
     @GetMapping("/flower/{id}/{colorId}")
     public FlowerDto getFlowerByIdAndColorId(@PathVariable int id, @PathVariable int colorId) {
-        return null;
+        FlowerDto flowerDto = catalog.getFlowerByIdAndColorId(id, colorId);
+        if (flowerDto == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return flowerDto;
     }
 
     @GetMapping("/search-flowers")
-    public List<FlowerCardDto> searchFlowers() {
-        return null;
+    public List<FlowerCardDto> searchFlowers(
+            @RequestParam(name = "name", required = false) String name,
+            @RequestParam(name = "minPrice", required = false) BigDecimal minPrice,
+            @RequestParam(name = "maxPrice", required = false) BigDecimal maxPrice,
+            @RequestParam(name = "familyNames", required = false) List<String> familyNames,
+            @RequestParam(name = "careTagNames", required = false) List<String> careTagNames,
+            @RequestParam(name = "colorNames", required = false) List<String> colorNames
+    ) {
+        if (minPrice != null && maxPrice != null && minPrice.compareTo(maxPrice) > 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        return catalog.searchFlowers(name, minPrice, maxPrice, familyNames, careTagNames, colorNames);
     }
 
     @GetMapping("/all-families")
     public List<String> getAllFamilies() {
-        // TODO("Вернуть список всех семейств")
-        return null;
+        return catalog.getAllFamilies();
     }
 
     @GetMapping("/all-colors")
     public List<String> getAllColors() {
-        return null;
+        return catalog.getAllColors();
     }
 
     @GetMapping("/all-care-tags")
     public List<String> getAllCareTags() {
-        return null;
+        return catalog.getAllCareTags();
     }
 
     @GetMapping("/price-range")
     public PriceRangeDto getPriceRange() {
-        return null;
+        return catalog.getPriceRange();
     }
 }
