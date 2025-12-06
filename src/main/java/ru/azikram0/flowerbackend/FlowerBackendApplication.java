5975be6ab56e35ba093ac9964384792d2aad29b1
Elizaveta <elisavetayapryntseva@yandex.ru>
2025-12-06 10:19:13 +0400

    Initial commit

---
+++ src/main/java/ru/azikram0/flowerbackend/FlowerBackendApplication.java
@@ -0,0 +1,11 @@
+package ru.azikram0.flowerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerBackendApplication {
+    void main() {
+        SpringApplication.run(FlowerBackendApplication.class);
+    }
+}
---
+++ src/test/java/ru/azikram0/flowerbackend/FlowerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.azikram0.flowerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
